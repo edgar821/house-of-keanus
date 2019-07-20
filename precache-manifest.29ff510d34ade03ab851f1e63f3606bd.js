ca51da501ce575bdf3587cdecdadb4e3b2664d25
Magpie <edchavez821@gmail.com>
2019-07-20 13:50:10 -0500

    Updates

---
+++ precache-manifest.29ff510d34ade03ab851f1e63f3606bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89257b49ed9b3a5881d480fb8ed348af",
+    "url": "/house-of-keanus/index.html"
+  },
+  {
+    "revision": "32f58094cb6898a45271",
+    "url": "/house-of-keanus/static/css/main.30a16591.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/house-of-keanus/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "32f58094cb6898a45271",
+    "url": "/house-of-keanus/static/js/main.615746f3.chunk.js"
+  },
+  {
+    "revision": "538d8802b5f828e649f9",
+    "url": "/house-of-keanus/static/js/runtime~main.ba5182d9.js"
+  }
+]);
